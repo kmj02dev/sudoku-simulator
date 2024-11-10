04667b7a4c852659e74c1876b0d4ad2f07ca4939
kmj02dev <person.social.assistance@gmail.com>
2024-11-10 17:30:25 +0900

    basic features complete

--- sudoku_simulator/sudoku_simulator.py
+++ sudoku_simulator/sudoku_simulator.py
@@ -3,21 +3,32 @@ import reflex as rx
 import asyncio
 import time
 from enum import Enum
+import threading
+import math
 
 # ----------------------------------------------------- Backend Area --------------------------------------------------
 class Solution:
     FAKE = "Fake"
     ConstraintPropagation = "Constratint Propatation"
-        
-class SudokuState(rx.State):
-    # 9x9 sudoku board class variable that all instances share
+    BackTracking = "Back Tracking"
+
+class State(rx.State):
+    # sudoku board vars
     board: List[List[int]] = [[0 for _ in range(9)] for _ in range(9)]
     selected_cell: tuple = (0, 0)
     original_board: List[List[int]] = [[0 for _ in range(9)] for _ in range(9)]
+    
+    # utility vars
     strategy : str = Solution.FAKE
+
     delay: float = 1
 
-    def init_board(cls):
+    start_time = 0
+    end_time = 0
+
+    count: int = 0
+
+    def init_board(self):
         base_puzzle = [
             [5, 3, 0, 0, 7, 0, 0, 0, 0],
             [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -29,54 +40,77 @@ class SudokuState(rx.State):
             [0, 0, 0, 4, 1, 9, 0, 0, 5],
             [0, 0, 0, 0, 8, 0, 0, 7, 9],
         ]
-        cls.board = base_puzzle
-        cls.original_board = [row[:] for row in base_puzzle]
+        self.board = base_puzzle
+        self.original_board = [row[:] for row in base_puzzle]
+        self.count = 0
+        self.start_time = 0
+        self.end_time = 0
 
-    def is_valid_move(cls, row: int, col: int, number: int) -> bool:
+    def is_valid_move(self, row: int, col: int, number: int) -> bool:
         # check row
-        if number in cls.board[row]:
+        if number in self.board[row]:
             return False
 
         # check column
-        if number in [cls.board[i][col] for i in range(9)]:
+        if number in [self.board[i][col] for i in range(9)]:
             return False
 
         # check 3x3 box
         box_row, box_col = 3 * (row // 3), 3 * (col // 3)
         for i in range(box_row, box_row + 3):
             for j in range(box_col, box_col + 3):
-                if cls.board[i][j] == number:
+                if self.board[i][j] == number:
                     return False
 
         return True
 
-    def insert_cell(cls, row: int, col: int, number: int):
-        if cls.original_board[row][col] == 0:
-            cls.board[row][col] = number
+    def insert_cell(self, row: int, col: int, number: int):
+        if self.original_board[row][col] == 0:
+            self.board[row][col] = number
+        
+        self.selected_cell = (row, col)
+        self.count += 1
 
-        cls.selected_cell = (row, col)
-        cls.common_update()
+    def delete_cell(self, row: int, col: int):
+        if self.original_board[row][col] == 0:
+            self.board[row][col] = 0
 
-    def delete_cell(cls, row: int, col: int):
-        if cls.original_board[row][col] == 0:
-            cls.board[row][col] = 0
+        self.selected_cell = (row, col)
+        self.count += 1
 
-        cls.selected_cell = (row, col)
-        cls.common_update()
-
-    def common_update(cls):
-        pass
-
-    def set_strategy(cls, strategy):
-        cls.strategy = strategy
-
-class FakeSudokuSolver(SudokuState):
-    def solve(self):
+# State를 통해 보드를 조작하고 분석할 수 있는 클래스입니다.
+class SudokuSolver(State):
+    async def solve(self):
+        self.start_time = time.time()
+        if self.strategy == Solution.FAKE:
+            self.fake_solve()
+        elif self.strategy == Solution.ConstraintPropagation:
+            self.constraint_propatation_solve()
+        elif self.strategy == Solution.BackTracking:
+            self.backtracking()
+        self.end_time = time.time()
+    
+    def fake_solve(self):
         for i in range(9):
             for j in range(9):
                 self.insert_cell(i, j, 1)
+                
+    def constraint_propatation_solve(self):
+        updated = False  # a flag to track if any updates
+        for row in range(9):
+            for col in range(9):
+                if self.board[row][col] == 0:
+                    candidates = self.find_candidates(row, col) 
+                    if len(candidates) == 1:
+                        # If there is exactly one candidate, fill the cell
+                        num = candidates.pop()
+                        self.insert_cell(row, col, num)
+                        updated = True
+        if updated:
+            self.constraint_propatation_solve()  # Continue until no more updates  
+        else:
+            self.backtracking()
 
-class ConstraintPropagationSudokuSolver(SudokuState):
     def find_candidates(self, row: int, col: int) -> set[int]:
         candidates = set(range(1, 10))
         # possible numbers that can be placed in the empty cell.
@@ -116,63 +150,40 @@ class ConstraintPropagationSudokuSolver(SudokuState):
     
     # Start backtracking from the top-left corner (0,0)
         return backtrack(0, 0)
-
-    def solve(self):
-        updated = False  # a flag to track if any updates
-        for row in range(9):
-            for col in range(9):
-                if self.board[row][col] == 0:
-                    candidates = self.find_candidates(row, col) 
-                    if len(candidates) == 1:
-                        # If there is exactly one candidate, fill the cell
-                        num = candidates.pop()
-                        self.insert_cell(row, col, num)
-                        updated = True
-        if updated:
-            self.solve()  # Continue until no more updates  
-        else:
-            self.backtracking()
+ 
 
 # ----------------------------------------------------- Frontend Area --------------------------------------------------
-def display_row(row):
-    return rx.hstack(
-        rx.foreach(
-            row,
-            lambda item: rx.box(
-                item,
-                border="1px solid black",
-                padding="0.5em",
-                margin="0",
-            ),
-        ),
-    )
-
-def button():
-    return rx.button(
-
-    )
 def index():
     return rx.container(
         rx.flex(
             rx.hstack(
-                rx.button("생성", on_click = SudokuState.init_board),
+                rx.text(f'{State.count}회'),
+                rx.text(f'{round((State.end_time - State.start_time)*10000) / 10000}초'),
+                rx.button("생성", on_click = State.init_board),
                 rx.select(
-                    [Solution.FAKE, Solution.ConstraintPropagation],
-                    value = SudokuState.strategy,
-                    on_change = SudokuState.set_strategy
+                    [Solution.FAKE, Solution.BackTracking, Solution.ConstraintPropagation],
+                    value = State.strategy,
+                    on_change = State.set_strategy
                 ),
-                rx.cond(
-                    (SudokuState.strategy == Solution.FAKE), # 조건
-                    rx.button("실행", on_click = FakeSudokuSolver.solve), # 참 
-                    rx.cond( # 거짓
-                        (SudokuState.strategy == Solution.ConstraintPropagation), # 조건
-                        rx.button("실행", on_click = ConstraintPropagationSudokuSolver.solve), # 참
-                        rx.button("실행") # 거짓
-                    )
-                )
+                rx.button("실행", on_click = SudokuSolver.solve)
             ),
             rx.vstack(
-                rx.foreach(SudokuState.board, display_row),
+                rx.foreach(
+                    State.board, 
+                    lambda row: 
+                        rx.hstack( 
+                            rx.foreach(
+                                row, 
+                                lambda item: 
+                                    rx.box(
+                                        item,
+                                        border="1px solid black",
+                                        padding="0.5em",
+                                        margin="0",
+                                    ),
+                            ),
+                        )
+                ),
                 align_items="center",
             ),
             center_content=True,
